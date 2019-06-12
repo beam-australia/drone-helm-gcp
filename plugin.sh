73d41e5d99be22ae9aa20b695f4819f05834b5a3
Andrew McLagan <andrewmclagan@gmail.com>
2019-06-12 13:09:18 +1000

    initial commit

--- plugin.sh
+++ plugin.sh
@@ -23,47 +23,43 @@ if [ "${PLUGIN_JSON_KEY:-}" ];then
     gcloud auth activate-service-account --key-file=/tmp/certs/svc_account.json
 fi
 
-# If there is no current context, get one.
-if [[ $(kubectl config current-context 2> /dev/null) == "" ]]; then
-    # This tries to read environment variables. If not set, it grabs from gcloud
-    cluster=${PLUGIN_CLUSTER:-$(gcloud config get-value container/cluster 2> /dev/null)}
-    region=${PLUGIN_REGION:-$(gcloud config get-value compute/region 2> /dev/null)}
-    zone=${PLUGIN_ZONE:-$(gcloud config get-value compute/zone 2> /dev/null)}
-    project=${PLUGIN_PROJECT:-$(gcloud config get-value core/project 2> /dev/null)}
+# This tries to read environment variables. If not set, it grabs from gcloud
+cluster=${PLUGIN_CLUSTER:-$(gcloud config get-value container/cluster 2> /dev/null)}
+region=${PLUGIN_REGION:-$(gcloud config get-value compute/region 2> /dev/null)}
+zone=${PLUGIN_ZONE:-$(gcloud config get-value compute/zone 2> /dev/null)}
+project=${PLUGIN_PROJECT:-$(gcloud config get-value core/project 2> /dev/null)}
 
-    function var_usage() {
-        cat <<EOF
+function var_usage() {
+    cat <<EOF
 No cluster is set. To set the cluster (and the region/zone where it is found), set the environment variables
   COMPUTE_REGION=<cluster region> (regional clusters)
   COMPUTE_ZONE=<cluster zone> (zonal clusters)
   CONTAINER_CLUSTER=<cluster name>
 EOF
-        exit 1
-    }
+    exit 1
+}
 
-    [[ -z "$cluster" ]] && var_usage
-    [ ! "$zone" -o "$region" ] && var_usage
+[[ -z "$cluster" ]] && var_usage
+[ ! "$zone" -o "$region" ] && var_usage
 
-    if [ -n "$region" ]; then
-      echo "Running: gcloud config set container/use_v1_api_client false"
-      gcloud config set container/use_v1_api_client false
-      echo "Running: gcloud beta container clusters get-credentials --project=\"$project\" --region=\"$region\" \"$cluster\""
-      gcloud beta container clusters get-credentials --project="$project" --region="$region" "$cluster" || exit
-    else
-      echo "Running: gcloud container clusters get-credentials --project=\"$project\" --zone=\"$zone\" \"$cluster\""
-      gcloud container clusters get-credentials --project="$project" --zone="$zone" "$cluster" || exit
-    fi
+if [ -n "$region" ]; then
+  echo "Running: gcloud config set container/use_v1_api_client false"
+  gcloud config set container/use_v1_api_client false
+  echo "Running: gcloud beta container clusters get-credentials --project=\"$project\" --region=\"$region\" \"$cluster\""
+  gcloud beta container clusters get-credentials --project="$project" --region="$region" "$cluster" || exit
 else
-  echo "Already have kube context"
+  echo "Running: gcloud container clusters get-credentials --project=\"$project\" --zone=\"$zone\" \"$cluster\""
+  gcloud container clusters get-credentials --project="$project" --zone="$zone" "$cluster" || exit
 fi
 
+echo "Running: kubectl config current-context"
 kubectl config current-context
 
 echo "Running: helm init --client-only"
 helm init --client-only
 
 if [ "$PLUGIN_DEBUG" = true ]; then
-    echo "helm upgrade $PLUGIN_RELEASE $PLUGIN_CHART \
+    echo "Running: helm upgrade $PLUGIN_RELEASE $PLUGIN_CHART \
             --install \
             --recreate-pods \
             --namespace=${PLUGIN_NAMESPACE:-default} \
