85e1bfe4965529f42257bebc05575032a8f8e41b
Andrew McLagan <andrewmclagan@gmail.com>
2019-06-12 15:53:51 +1000

    initial commit

--- plugin.sh
+++ plugin.sh
@@ -3,42 +3,25 @@
 set -e
 set -v
 
-###
-# PLUGIN_JSON_KEY
-# PLUGIN_CLUSTER
-# PLUGIN_COMPUTE_REGION
-# PLUGIN_COMPUTE_ZONE
-# PLUGIN_PROJECT
-# PLUGIN_DEBUG
-# PLUGIN_RELEASE
-# PLUGIN_RELEASE
-# PLUGIN_CHART
-# PLUGIN_NAMESPACE
-# PLUGIN_VALUES
-# PLUGIN_VERSION
-###
+GCP_KEY=${PLUGIN_JSON_KEY:-"{}"}
+PROJECT=${PLUGIN_PROJECT:-$PROJECT}
+ZONE=${PLUGIN_ZONE:-$ZONE}
+CLUSTER=${PLUGIN_CLUSTER:-$CLUSTER}
+NAMESPACE=${PLUGIN_NAMESPACE:-default}
+VERSION=${PLUGIN_VERSION:-latest}
 
 echo "Setting auth key from environment"
-echo "${PLUGIN_JSON_KEY:-"{}"}" > /var/svc_account.json
+echo "$GCP_KEY" > /var/svc_account.json
 cat /var/svc_account.json
 gcloud auth activate-service-account --key-file=/var/svc_account.json
 
 echo "Setting cluster context"
 gcloud config set container/use_v1_api_client false
-gcloud beta container clusters get-credentials --project="$PLUGIN_PROJECT" --zone="$PLUGIN_ZONE" "$PLUGIN_CLUSTER" || exit
+gcloud beta container clusters get-credentials --project="$PROJECT" --zone="$ZONE" "$CLUSTER" || exit
 kubectl config current-context
 
 echo "Initializing helm"
 helm init --client-only
-
-if [ "$PLUGIN_DEBUG" = true ]; then
-    echo "Running: helm upgrade $PLUGIN_RELEASE $PLUGIN_CHART \
-            --install \
-            --recreate-pods \
-            --namespace=${PLUGIN_NAMESPACE:-default} \
-            --values=${PLUGIN_CHART}/values.yaml,${PLUGIN_CHART}/${PLUGIN_VALUES} \
-            --set version=${PLUGIN_VERSION}"
-fi
 
 helm upgrade $PLUGIN_RELEASE $PLUGIN_CHART \
   --install \
